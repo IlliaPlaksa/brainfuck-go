68abc8449b7414aca699d180ed359fa26ec9869d
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-20 22:24:14 +0200

    Add documentation for brainfuck package

---
+++ brainfuck/doc.go
@@ -0,0 +1,7 @@
+/*
+	Package provides a functionality of BrainFuck programming language interpreter
+Example usage:
+		brainfuck.Interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+		"Hello, world!" program in BrainFuck
+*/
+package brainfuck
