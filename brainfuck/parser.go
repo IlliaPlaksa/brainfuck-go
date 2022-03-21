827c079696506697de112a9baabee1ccc22e2b38
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-21 18:30:47 +0200

    Make commands structs private

--- brainfuck/parser.go
+++ brainfuck/parser.go
@@ -22,20 +22,20 @@ func (p *parser) parse(input string) []command {
 func (p *parser) parseInstruction(char rune) {
 	switch char {
 	case '+':
-		p.stack[p.ptr] = append(p.stack[p.ptr], Increment{})
+		p.stack[p.ptr] = append(p.stack[p.ptr], increment{})
 	case '-':
-		p.stack[p.ptr] = append(p.stack[p.ptr], Decrement{})
+		p.stack[p.ptr] = append(p.stack[p.ptr], decrement{})
 	case '>':
-		p.stack[p.ptr] = append(p.stack[p.ptr], MoveForward{})
+		p.stack[p.ptr] = append(p.stack[p.ptr], moveForward{})
 	case '<':
-		p.stack[p.ptr] = append(p.stack[p.ptr], MoveBackward{})
+		p.stack[p.ptr] = append(p.stack[p.ptr], moveBackward{})
 	case '.':
-		p.stack[p.ptr] = append(p.stack[p.ptr], Print{})
+		p.stack[p.ptr] = append(p.stack[p.ptr], output{})
 	case '[':
 		p.stack = append(p.stack, []command{})
 		p.ptr++
 	case ']':
-		p.stack[p.ptr-1] = append(p.stack[p.ptr-1], Loop{p.stack[p.ptr]})
+		p.stack[p.ptr-1] = append(p.stack[p.ptr-1], loop{p.stack[p.ptr]})
 		p.stack = p.stack[:p.ptr]
 		p.ptr--
 	}
