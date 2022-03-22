c16f8068d4335fb72610877a641d41cc2eee5745
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-22 17:12:58 +0200

    Add stdout capturing tests

--- brainfuck/executor_test.go
+++ brainfuck/executor_test.go
@@ -1,21 +1,56 @@
 package brainfuck
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
+
+func captureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
 
 func TestInterpret(t *testing.T) {
 	type args struct {
 		program string
 	}
 	tests := []struct {
-		name string
-		args args
+		name     string
+		args     args
+		expected string
 	}{
-		{"Hello, world!", args{"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."}},
-		{"Dirty hello world", args{"+++vsdfv+++++[>++++s[>++>++a+>+v++>+<a<<<-]>+>d+>->>+[w!<]<-]>>.>--\t-.++++vdf+++..+++.>>.<-.<.++efb +.------.-----\n---.>>+.>++."}},
+		{
+			name:     "Hello world!",
+			args:     args{"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."},
+			expected: "Hello World!\n",
+		},
+
+		{
+			name:     "Dirty hello world",
+			args:     args{"+++vsdfv+++++[>++++s[>++>++a+>+v++>+<a<<<-]>+>d+>->>+[w!<]<-]>>.>--\t-.++++vdf+++..+++.>>.<-.<.++efb +.------.-----\n---.>>+.>++."},
+			expected: "Hello World!\n"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Interpret(tt.args.program)
+			out := captureStdout(func() {
+				Interpret(tt.args.program)
+			})
+
+			if out != tt.expected {
+				t.Errorf("Expected %v but have %v", tt.expected, out)
+			}
 		})
 	}
 }
