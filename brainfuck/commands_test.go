45b0b7e17fae46e4bc89736d74e86d79f967cb61
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-22 16:41:34 +0200

    Add simple commands tests

--- brainfuck/commands_test.go
+++ brainfuck/commands_test.go
@@ -5,6 +5,52 @@ import (
 	"testing"
 )
 
+func TestSimpleCommands_Execute(t *testing.T) {
+	type args struct {
+		memory *memory
+	}
+	tests := []struct {
+		name     string
+		command  command
+		args     args
+		expected memory
+	}{
+		{
+			name:     "should increment buffer's cell with index [0] by one",
+			command:  increment{},
+			args:     args{memory: newMemory([]byte{0}, 0)},
+			expected: *newMemory([]byte{1}, 0),
+		},
+		{
+			name:     "should decrement buffer's cell with index [0] by one",
+			command:  decrement{},
+			args:     args{memory: newMemory([]byte{3}, 0)},
+			expected: *newMemory([]byte{2}, 0),
+		},
+		{
+			name:     "should move memory pointer to the right",
+			command:  moveForward{},
+			args:     args{memory: newMemory([]byte{0}, 0)},
+			expected: *newMemory([]byte{0, 0}, 1),
+		},
+		{
+			name:     "should move memory pointer to the left",
+			command:  moveBackward{},
+			args:     args{memory: newMemory([]byte{0, 0, 0}, 2)},
+			expected: *newMemory([]byte{0, 0, 0}, 1),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.command.Execute(tt.args.memory)
+			if !reflect.DeepEqual(*tt.args.memory, tt.expected) {
+				t.Errorf("Expected %v \n but have %v", tt.expected, *tt.args.memory)
+			}
+		})
+	}
+}
+
 func TestLoop_execute(t *testing.T) {
 	type fields struct {
 		commands []command
