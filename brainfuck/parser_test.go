0663c884bb3751207a5287de5c7614736dcdfdff
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-25 12:42:14 +0200

    Add parser_test.go

---
+++ brainfuck/parser_test.go
@@ -0,0 +1,44 @@
+package brainfuck
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParser_parse(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected []command
+	}{
+		{
+			name: "should parse proper input",
+			args: args{"[+-><.]"},
+			expected: []command{
+				loop{
+					[]command{
+						increment{}, decrement{}, moveForward{}, moveBackward{}, output{},
+					},
+				},
+			},
+		},
+		{
+			name:     "should parse input with third party symbols",
+			args:     args{"+#-@><)."},
+			expected: []command{increment{}, decrement{}, moveForward{}, moveBackward{}, output{}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := newParser()
+			commands := p.parse(tt.args.input)
+			if !reflect.DeepEqual(commands, tt.expected) {
+				t.Errorf("Expected %v \n but have %v", tt.expected, commands)
+			}
+		})
+	}
+}
