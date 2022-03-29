9fd352aa49c2cc2462788957d13b4303c52f242a
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-03-30 00:03:37 +0300

    Update brainfuck/doc.go

--- brainfuck/doc.go
+++ brainfuck/doc.go
@@ -1,6 +1,12 @@
 /*
-Package brainfuck provides a functionality of interpreter of BrainFuck programming language
-Example usage:
+Package brainfuck provides a functionality of interpreter of BrainFuck programming language.
+
+Available instructions: "+", "-", ">", "<", "[", "]", ".".
+
+	Note:
+Characters in input BrainFuck program that does not match any available instruction will be ignored.
+
+	Example usage:
 		"Hello, world!" program in BrainFuck
 		brainfuck.Interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
 */
