24439e07317c099ccf286da5a7e813c81af148a9
IlliaPlaksa <illia.plaksa@teamdev.com>
2022-04-11 14:36:31 +0300

    Update doc.go

--- brainfuck/doc.go
+++ brainfuck/doc.go
@@ -1,7 +1,11 @@
 /*
 Package brainfuck provides a functionality of interpreter of BrainFuck programming language.
 
-Available instructions: "+", "-", ">", "<", "[", "]", ".".
+Available instructions:
+
+base: "+", "-", ">", "<", "[", "]", ".".
+
+custom: "$" - increment by five
 
 	Note:
 Characters in input BrainFuck program that does not match any available instruction will be ignored.
